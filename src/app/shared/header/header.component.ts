7c3bd13d43cf73efada2e18b9ad4abc2934c5e7c
ALLOUDH <zeus_apolo85@hotmail.com>
2024-10-12 01:21:11 -0500

    Correcion de errores (SPRINT 1)
    Se corrigieron errores en las rutas de algunas dependencias que daban error en la consolola
    
    Se implemento el nombre y rol del usuario loggeado en el header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AccesoService } from '../../services/acceso.service';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
@@ -8,15 +9,35 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  username: string | null = null;
+  userRole: string | null = null;
   constructor(
     private router: Router,
     private bsModalRecoverPass: BsModalRef,
     private modalService: BsModalService,
+    private accesoService: AccesoService,
   ) {
 
   }
+
+  ngOnInit(): void {
+    this.UserNameRol();
+  }
+
   CerrarSesion() {
     localStorage.removeItem('usertoken');
     this.router.navigate(['/login']);
+  }
+
+  UserNameRol() {
+    if ((this.accesoService.getUserName() === null) || (this.accesoService.getUserRole() === null)) {
+      this.username = "Usuario";
+      this.userRole = "Rol";
+      console.log("No se encontraron los datos del usuario");
+    }else {
+      this.username = this.accesoService.getUserName()+" "+this.accesoService.getUserSurname();
+      this.userRole = this.accesoService.getUserRole();
+    }
   }
 }
